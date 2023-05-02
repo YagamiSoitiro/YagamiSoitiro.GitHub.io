c0c19a0207d30523176ab452726dccf5633f29a7
YagamiSoitiro <132275346+YagamiSoitiro@users.noreply.github.com>
2023-05-02 15:22:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-var of = true;
 async onload = () => {
-  alert('a');
-  safari.alarms.create("onof", { "periodInMinutes": 0.0017 });
-  setInterval(() => {
-    of = true;
-  }, 0.1);
-  safari.alarms.onAlarm.addListener(function (alarm) {
-    document.querySelector('div').innerHTML = '<div>' + of + '</div>';
-    if(of == true) of = false
-  });
 }
